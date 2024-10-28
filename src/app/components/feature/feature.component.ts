e4fbae1287c0acfe8b28637b9d2d562d68213b2d
3shadow7 <the.crazy.shadow7@gmail.com>
2024-10-28 19:51:10 +0200

    7 commit

--- src/app/components/feature/feature.component.ts
+++ src/app/components/feature/feature.component.ts
@@ -1,8 +1,12 @@
+import { rowData } from './../../sharing/constant';
 import { Component, ViewEncapsulation } from '@angular/core';
 import { AgGridAngular, ICellRendererAngularComp } from 'ag-grid-angular';
 import { ColDef, ICellComp, ICellRendererParams } from 'ag-grid-community';
+
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-quartz.css';
+// import 'ag-grid-enterprise';
+
 
 @Component({
   selector: 'app-feature',
@@ -11,7 +15,50 @@ import 'ag-grid-community/styles/ag-theme-quartz.css';
   templateUrl: './feature.component.html',
   styleUrl: './feature.component.scss',
 })
+
+
 export class FeatureComponent implements ICellRendererAngularComp {
+
+  success_class:string[] = [
+    'Yes',
+    'Extensive',
+    'Active',
+    'Monthly',
+    'All Android devices',
+    'High',
+    'High performance',
+    'Available',
+    '24/7',
+    'Regular',
+    'Cloud & Local',
+    'Singleplayer & Multiplayer',
+    'Available',
+    '24/7',
+    'Regular',
+  ];
+
+  warning_class:string[] = [
+    'Medium',
+    'Optional',
+    'Bi-Monthly',
+    'Growing',
+    'iOS 12 and above',
+    'Moderate',
+    'Business hours',
+    'Variable',
+    'Occasional',
+    'Cloud only',
+    'Singleplayer only',
+  ];
+
+  danger_class :string[] = [
+    'No',
+    'Limited',
+    'Modern browsers',
+    'Coming soon',
+  ];
+
+
   agInit(params: ICellRendererParams): void {
     throw new Error('Method not implemented.');
   }
@@ -19,76 +66,19 @@ export class FeatureComponent implements ICellRendererAngularComp {
     return true;
   }
 
+
   defultCol: ColDef = {
     flex: 4,
     cellClassRules: {
-      'bg-success': (p) => {
-        let substrings = [
-          'Yes',
-          'Extensive',
-          'Active',
-          'Monthly',
-          'All Android devices',
-          'High',
-          'High performance',
-          'Available',
-          '24/7',
-          'Regular',
-          'Cloud & Local',
-          'Singleplayer & Multiplayer',
-          'Available',
-          '24/7',
-          'Regular',
-        ];
-        let allIncluded = substrings.some((sub) => p.value === sub);
-        if (allIncluded) {
-          return true;
-        }
-        console.log(
-          p.value,
-          substrings.some((sub) => p.value === sub)
-        );
-        return false;
-      },
-      'bg-warning': (p) => {
-        let substrings = [
-          'Medium',
-          'Optional',
-          'Bi-Monthly',
-          'Growing',
-          'iOS 12 and above',
-          'Moderate',
-          'Business hours',
-          'Variable',
-          'Occasional',
-          'Cloud only',
-          'Singleplayer only',
-        ];
-        let allIncluded = substrings.some((sub) => p.value === sub);
-        if (allIncluded) {
-          return true;
-        }
-        console.log(
-          p.value,
-          substrings.some((sub) => p.value === sub)
-        );
-        return false;
-      },
-      'bg-danger': (p) => {
-        let substrings = ['No', 'Limited', 'Modern browsers', 'Coming soon'];
-        let allIncluded = substrings.some((sub) => p.value === sub);
-        if (allIncluded) {
-          return true;
-        }
-        console.log(
-          p.value,
-          substrings.some((sub) => p.value === sub)
-        );
-        return false;
-      },
+      'bgsuccess': p => this.success_class.some((sub) => p.value === sub) ? true : false //* check if the value of cell is match one of the success_class then put the success class on it
+      ,
+      'bgwarning': p => this.warning_class.some((sub) => p.value === sub) ? true : false
+      ,
+      'bgdanger': p => this.danger_class.some((sub) => p.value === sub) ? true : false
+      ,
     },
   };
-
+ //! header of table
   ColDef: ColDef[] = [
     { headerName: 'Feature', field: 'feature', cellClass: 'first-column' },
     { headerName: 'Android', field: 'android' },
@@ -96,144 +86,6 @@ export class FeatureComponent implements ICellRendererAngularComp {
     { headerName: 'Web', field: 'web' },
   ];
 
-  rowData = [
-    {
-      feature: 'Graphics Quality',
-      android: 'High',
-      ios: 'High',
-      web: 'Medium',
-    },
-    {
-      feature: 'Multiplayer Support',
-      android: 'Yes',
-      ios: 'Yes',
-      web: 'No',
-    },
-    {
-      feature: 'Touch Controls',
-      android: 'Yes',
-      ios: 'Yes',
-      web: 'No',
-    },
-    {
-      feature: 'Cross-Platform Play',
-      android: 'Yes',
-      ios: 'Yes',
-      web: 'Yes',
-    },
-    {
-      feature: 'In-Game Purchases',
-      android: 'Yes',
-      ios: 'Yes',
-      web: 'Optional',
-    },
-    {
-      feature: 'Achievements',
-      android: 'Yes',
-      ios: 'Yes',
-      web: 'Limited',
-    },
-    {
-      feature: 'Offline Mode',
-      android: 'Yes',
-      ios: 'Yes',
-      web: 'No',
-    },
-    {
-      feature: 'Updates Frequency',
-      android: 'Monthly',
-      ios: 'Monthly',
-      web: 'Bi-Monthly',
-    },
-    {
-      feature: 'Customization Options',
-      android: 'Extensive',
-      ios: 'Extensive',
-      web: 'Limited',
-    },
-    {
-      feature: 'User Community',
-      android: 'Active',
-      ios: 'Active',
-      web: 'Growing',
-    },
-    {
-      feature: 'Device Compatibility',
-      android: 'All Android devices',
-      ios: 'iOS 12 and above',
-      web: 'Modern browsers',
-    },
-    {
-      feature: 'Social Media Integration',
-      android: 'Yes',
-      ios: 'Yes',
-      web: 'Limited',
-    },
-    {
-      feature: 'Performance Optimization',
-      android: 'High performance',
-      ios: 'High performance',
-      web: 'Variable',
-    },
-    {
-      feature: 'Event Participation',
-      android: 'Yes',
-      ios: 'Yes',
-      web: 'No',
-    },
-    {
-      feature: 'Tutorials and Guides',
-      android: 'Available',
-      ios: 'Available',
-      web: 'Coming soon',
-    },
-    {
-      feature: 'Character Customization',
-      android: 'Extensive',
-      ios: 'Extensive',
-      web: 'Moderate',
-    },
-    {
-      feature: 'Graphic Settings Adjustments',
-      android: 'Yes',
-      ios: 'Yes',
-      web: 'Limited',
-    },
-    {
-      feature: 'Customer Support',
-      android: '24/7',
-      ios: '24/7',
-      web: 'Business hours',
-    },
-    {
-      feature: 'Community Events',
-      android: 'Regular',
-      ios: 'Regular',
-      web: 'Occasional',
-    },
-    {
-      feature: 'Save Game Progress',
-      android: 'Cloud & Local',
-      ios: 'Cloud & Local',
-      web: 'Cloud only',
-    },
-    {
-      feature: 'Game Modes',
-      android: 'Singleplayer & Multiplayer',
-      ios: 'Singleplayer & Multiplayer',
-      web: 'Singleplayer only',
-    },
-    {
-      feature: 'Beta Testing Opportunities',
-      android: 'Yes',
-      ios: 'Yes',
-      web: 'No',
-    },
-    {
-      feature: 'Accessibility Features',
-      android: 'Yes',
-      ios: 'Yes',
-      web: 'Limited',
-    },
-  ];
+ //! rowData of table
+ RowDef = rowData ;
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { Component, OnInit, inject, AfterViewInit } from '@angular/core';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +14,79 @@ export class HomeComponent implements OnInit{
   getstarted(){
     this.router.navigate(['/download']);
   }
-
   ngOnInit(): void {
 
+
+    var chartDom = document.getElementById('main');
+    var myChart = echarts.init(chartDom);
+
+
+   var option = {
+     backgroundColor: '#fff',
+     title: {
+       text: 'downloads state',
+       left: 'center',
+       top: 20,
+       textStyle: {
+         color: '#bbb'
+       }
+     },
+     tooltip: {
+       trigger: 'item'
+     },
+     visualMap: {
+       show: false,
+       min: 80,
+       max: 600,
+       inRange: {
+         colorLightness: [0, 1]
+       }
+     },
+     series: [
+       {
+         name: 'Downloads From',
+         type: 'pie',
+         radius: '55%',
+         center: ['50%', '50%'],
+         data: [
+           { value: 335, name: 'USA' },
+           { value: 310, name: 'GERMAN' },
+           { value: 274, name: 'INDIA' },
+           { value: 235, name: 'LIBYA' },
+           { value: 400, name: 'UK' }
+         ].sort(function (a, b) {
+           return a.value - b.value;
+         }),
+         roseType: 'radius',
+         label: {
+           color: 'rgba(0, 0, 0, 0.3)'
+         },
+         labelLine: {
+           lineStyle: {
+             color: 'rgba(0, 0, 0, 0.3)'
+           },
+           smooth: 0.2,
+           length: 10,
+           length2: 20
+         },
+         itemStyle: {
+           color: '#eeeeee',
+           shadowBlur: 200,
+           shadowColor: 'rgba(0, 0, 0, 0.5)'
+         },
+         animationType: 'scale',
+         animationEasing: 'elasticOut',
+         animationDelay: function (idx:any) {
+           return Math.random() * 800;
+         }
+       }
+     ]
+   };
+
+   option && myChart.setOption(option);
+
+
   }
+
 
 }
